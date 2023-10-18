c4b203b419d27a8036af5d58c6c3193c56e0d5fc
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2023-10-18 18:08:58 +0300

    update navbar styles

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex gap-4 mx-auto container px-8 mt-8">
       <div className="w-1/5">
-        <FilterRentals  />
+        Filter Rentals
       </div>
       <div className="w-4/5">
         <Rentals />
