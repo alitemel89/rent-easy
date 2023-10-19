092eb32e5435aa848aff0d3cfdb111fff46ad299
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2023-10-19 12:08:56 +0300

    add reacticons to rental card

--- client/app/components/RentalCard.tsx
+++ client/app/components/RentalCard.tsx
@@ -1,20 +1,20 @@
 // RentalCard.tsx
-"use client"
-import Link from 'next/link';
-import React from 'react';
+"use client";
+import Link from "next/link";
+import React from "react";
+import { GiBathtub, GiBed } from "react-icons/gi";
 
 export interface Rental {
-    _id: string;
-    title: string;
-    description: string;
-    price: number;
-    bedrooms: number;
-    bathrooms: number;
-    location: string;
-    userRef: string;
-    __v: number;
-  }
-
+  _id: string;
+  title: string;
+  description: string;
+  price: number;
+  bedrooms: number;
+  bathrooms: number;
+  location: string;
+  userRef: string;
+  __v: number;
+}
 
 interface RentalCardProps {
   rental: Rental; // Assuming 'Rental' is the type/interface for a single property
@@ -23,17 +23,29 @@ interface RentalCardProps {
 const RentalCard: React.FC<RentalCardProps> = ({ rental }) => {
   return (
     <Link href={`/rentals/${rental._id}`}>
-        <div className="bg-white rounded-3xl p-6 shadow-xl">
-          <h2 className="text-xl font-semibold text-blue-950 mb-2">{rental.title}</h2>
-          <p className="text-gray-500 text-sm mb-2">{rental.location}</p>
-          <p className="text-gray-700 mb-4">{rental.description}</p>
-          <div className="flex justify-between">
-            <p className="text-blue-950 text-lg font-extrabold">${rental.price} / month</p>
-            <p className="text-indigo-500">
-              {rental.bedrooms} BR | {rental.bathrooms} BA
-            </p>
+      <div className="bg-white rounded-3xl p-6 shadow-xl">
+        <h2 className="text-xl font-semibold text-blue-950 mb-2">
+          {rental.title}
+        </h2>
+        <p className="text-gray-500 text-sm mb-2">{rental.location}</p>
+        <p className="text-gray-700 mb-4">{rental.description}</p>
+        <div className="flex justify-between">
+          <p className="text-blue-950 text-lg font-extrabold">
+            ${rental.price} / month
+          </p>
+          <div className="flex items-center space-x-2">
+            <div className="flex items-center space-x-2">
+              <span>{rental.bathrooms}</span>
+              <GiBathtub size={20} className="text-indigo-500"/>
+            </div>
+            <span>|</span>
+            <div className="flex items-center space-x-2">
+              <span>{rental.bedrooms}</span>
+              <GiBed size={20} className="text-indigo-500"/>
+            </div>
           </div>
         </div>
+      </div>
     </Link>
   );
 };
