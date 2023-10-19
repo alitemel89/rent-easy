fd06c762bb328704b8830930f8d261a4005b6091
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2023-10-19 14:42:40 +0300

    update page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -14,11 +14,11 @@ export default function Home() {
     setFilter(newFilter);
   };
   return (
-    <div className="md:flex gap-4 mx-auto p-8 bg-blue-50 min-h-screen">
-      <div className="md:w-1/3 w-full mb-4">
+    <div className="md:flex gap-4 mx-auto p-6 bg-blue-50 min-h-screen">
+      <div className="md:w-1/5 w-full mb-4">
         <FilterRentals onFilterChange={handleFilterChange} />
       </div>
-      <div className="md:w-2/3 w-full">
+      <div className="md:w-4/5 w-full">
         <Rentals filter={filter }/>
       </div>
     </div>
